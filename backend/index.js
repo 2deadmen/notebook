4bda84526db2b715a2530b58f8b9c4d703ebbcbd
2deadmen <kartikhegde.2002@gmail.com>
2022-10-04 21:17:25 +0530

    hg

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,17 @@
-const connectToMongo=require('./db');
-const express = require('express')  
+const connectToMongo = require('./db');
+const express = require('express')
+
 connectToMongo();
-
-
-
 const app = express()
 const port = 3001
 
 app.use(express.json())
-//index route
-app.get('/',(req,res)=>{
-  obj={
-    n:"xdbd"
-  }
-  res.send(obj)
-})
 
-//routes
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,15 @@ const express=require('express');
 const router=express.Router();
 const User = require('../models/User') 
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser')
+const  jwt_secret ="this is not a secret"
 
 
 
-router.post('/',
+
+router.post('/signup',
 //validators
 [
     body('password',"should be atleast 8 characters").isLength({ min: 8 }),
@@ -23,17 +28,83 @@ try{
         if(user)
          { return res.status(400).json({error:"user already exists "});
     }
+  //password hashing and salting
+    const salt= await bcrypt.genSalt(10);
+     secPass= await bcrypt.hash(req.body.password,salt)
+
+
     //creating new user
          user= await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
           })
-          res.send({"ok":1})}catch(error){
+        //authtoken
+          const data={
+            user:{
+              id:user.id
+            }
+
+          }
+        const authToken=jwt.sign(data,jwt_secret)
+        res.json({authToken})
+             
+        
+        }catch(error){
             console.error(error)
-            res.status(500)
+            res.status(500).json({error:"internal server error"})
 
           }
         })
+    router.post('/login',[
+    body('password',"should be atleast 8 characters").isLength({ min: 8 }),
+    body('email','enter a valid email').isEmail()] ,async (req,res)=>{
+
+      const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
+
+        const {email,password}=req.body;
+        try{
+          const user= await User.findOne({email:req.body.email});
+          if(!user){
+         return res.status(400).json({error:"invalid credentials"})
+
+          }
+         
+          const passcompare= await bcrypt.compare(password,user.password)
+          if (!passcompare){
+            return res.status(400).json({error:"invalid credentials"})
+
+          }
+          const data={
+            user:{
+              id:user.id
+            }
+
+          }
+        const authToken=jwt.sign(data,jwt_secret)
+        res.json({authToken})
+             
+        }catch(error){
+          console.error(error)
+          res.status(500).json({error:"internal server error"})
+
+
+        }
+    })
+    router.post('/getuser' ,fetchuser,async (req,res)=>{
+     
+        try {
+          userId=req.user.id;
+          const user=await User.findById(userId).select("-password")
+          res.send(user)
+        } catch (error) {
+          console.error(error)
+          res.status(500).json({error:"internal server error"})          
+        }
+
+      }  )
 
 module.exports=router
